e5d23a551238da843d59a8955e19d0ef29e6ccf2
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-04-16 11:27:11 +0000

    datatypes

---
+++ Basics/conversion.js
@@ -0,0 +1,29 @@
+let score = false
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1 & false =>0
+
+let isLoggedIn = 0
+let booleanisloggedIn = Boolean(isLoggedIn)
+console.log(booleanisloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Akash" => true
+
+
+
+let somenumber = 33
+let stringNumber = String(somenumber) //helps to convert the datatypes
+console.log(stringNumber)
+console.log(typeof stringNumber)
