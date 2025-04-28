07563dbdea1b3ebffd54a1e3f3a6956745ef4b48
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-04-28 13:53:31 +0000

    datatypes summary

--- Basics/datatypes_summary.js
+++ Basics/datatypes_summary.js
@@ -31,3 +31,30 @@ const myFunction = function(){
 console.log(typeof myFunction)
 console.log(typeof myObj)
 myFunction()
+
+
+
+// *************************************************************************************************************************
+// Stack and Heap Memory
+// Stack (Primitive) , Heap (Non-Primitive)
+// stack gives copy of original values and you change value in the copied content not in the original content
+// Heap gives the references of original content so when you change the another value it changes the original content 
+
+let myYoutubeChannelName = "Akashvohragmaildotcom"
+let anothername = myYoutubeChannelName
+anothername = "Amanvohragmail"
+console.log(myYoutubeChannelName);
+console.log(anothername);
+
+
+let userOne = {
+    email :"akash123@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email ="akash@gndec.ac.in"
+
+console.log(userOne.email);
+console.log(userTwo.email);
