b1f79b8a0f2af7dce9af1384ccbe4c602926e264
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-05-01 01:38:01 +0000

    arrow function

---
+++ 03_Basics/Arrow_fun.js
@@ -0,0 +1,22 @@
+const user ={
+    username : "Akash",
+    price :999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username} , Welcome to Website`);
+        console.log(this)
+    }
+}
+// user.welcomeMessage()
+// user.username = "Aman Vohra"
+// user.welcomeMessage()
+
+// console.log(this)
+
+
+// function chai (){
+//     let username = "Akash"
+//     console.log(this.username);
+// }
+// chai()
+
