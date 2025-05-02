c629b9aabd729f45000a5e71c5ce76d8079dc340
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-05-02 03:29:19 +0000

    Arrow Function

--- 03_Basics/Arrow_fun.js
+++ 03_Basics/Arrow_fun.js
@@ -20,3 +20,25 @@ const user ={
 // }
 // chai()
 
+// const  chai = function(){
+//     let username = "Akash"
+//    console.log(this.username);
+// }
+const  chai = () => {
+    let username = "Akash"
+   console.log(this);
+}
+// chai()
+
+// const addTwo = (num1, num2)=>{
+//     return num1 + num2
+// }
+// const addTwo = (num1, num2)=> num1 + num2
+const addTwo = (num1, num2)=> (num1 + num2)
+
+console.log(addTwo(3,4))
+
+
+// const myArray= [1,2,3,4,5]
+// myArray.forEach(()=>())
+    
